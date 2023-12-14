410d6f12baff36b07fd0817cff7ed1c9ef512bfc
BoyanKardzhiev <b.r.kardzhiev@student.tue.nl>
2023-12-14 15:46:30 +0200

    Bug fix

--- ExampleAR/Assets/Scripts/DistanceBar1.cs
+++ ExampleAR/Assets/Scripts/DistanceBar1.cs
@@ -17,9 +17,9 @@ public class DistanceBar1 : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Something");
         if(other.gameObject.GetComponent<Camera>() != null)
         {
+            Debug.Log("Something");
             DistanceBar.SetDistance(1);
             gameObject.SetActive(false);
         }
